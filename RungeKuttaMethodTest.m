2eee7eaba3ba08b6d696b98665b745a24a13f678
ajinkyagorad <ajinkyagorad@gmail.com>
2016-05-30 20:47:11 +0530

    tested Runge-Kutta 2nd order

--- RungeKuttaMethodTest.m
+++ RungeKuttaMethodTest.m
@@ -1,6 +1,20 @@
 %Implementation of Runge Kutta Method
 %---------------------------------
-% y(i+1) = y(i) + h * (Weighted Avg of slopes)
+% y(i+1) = y(i) +(Weighted Avg of slopes*h)
+% K1*W1+K2*W2+K3*W3+...+Kv*Wv : v-stage method
+% K1 = h*f(xi,yi)
+% K2 = h*f(xi+c2*h,yi+a21*K1)
+%For  2 stage Method
+% y(i+1) = y(i)+ h f(xi+0.5h,yi+0.5*h*f(xi,yi))
 %-----------------------------------
-
-
+clear;
+h = 0.01;
+y = 0; % initial value
+y_old = y; 
+A=0;
+for n = 0:1000;
+    y = y_old+h*f(n*h+0.5*h,y_old*h*f(n*h,y_old));
+    A=[A y];
+end
+plot(A);
+    
